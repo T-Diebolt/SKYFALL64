4ff52a18986e8e3917930d3c574d201af8295c37
T-Diebolt <theodieb542@outlook.com>
2022-05-26 14:04:53 -0400

    Add project files.

---
+++ SKYFALL64/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SKYFALL64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameEngine = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.difficultyLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameEngine
+            // 
+            this.gameEngine.Tick += new System.EventHandler(this.gameEngine_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("OCR A Extended", 63.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Lime;
+            this.titleLabel.Location = new System.Drawing.Point(12, 137);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(576, 90);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "SKYFALL64";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subtitleLabel.Font = new System.Drawing.Font("OCR A Extended", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtitleLabel.ForeColor = System.Drawing.Color.Lime;
+            this.subtitleLabel.Location = new System.Drawing.Point(12, 245);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(576, 129);
+            this.subtitleLabel.TabIndex = 1;
+            this.subtitleLabel.Text = "Press space to play,\r\nd to change difficulty,\r\nor escape to exit.";
+            this.subtitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.BackColor = System.Drawing.Color.Transparent;
+            this.difficultyLabel.Font = new System.Drawing.Font("OCR A Extended", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difficultyLabel.ForeColor = System.Drawing.Color.Lime;
+            this.difficultyLabel.Location = new System.Drawing.Point(12, 434);
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(576, 129);
+            this.difficultyLabel.TabIndex = 2;
+            this.difficultyLabel.Text = "Difficulty: Medium";
+            this.difficultyLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("OCR A Extended", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.Lime;
+            this.scoreLabel.Location = new System.Drawing.Point(463, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(140, 75);
+            this.scoreLabel.TabIndex = 3;
+            this.scoreLabel.Text = "Score:\r\n0";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.difficultyLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameEngine;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Label difficultyLabel;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ SKYFALL64/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace SKYFALL64
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+        int numHold = 0;
+
+        Rectangle coverBack = new Rectangle(450, 0, 150, 600);
+        Rectangle coverLine = new Rectangle(450, 0, 5, 600);
+        Rectangle[] player = new Rectangle[4];
+        int playerSpeed = 50;
+
+        List<Rectangle> shots = new List<Rectangle>();
+        int shotSpeed = 50;
+
+        Rectangle[] squares = new Rectangle[99];
+        int obstacleCount = 0;
+        int obstacleSpeed = 0;
+        int[] squaresActive = new int[99];
+
+        int playerScore = 0;
+
+        string difficulty = "MEDIUM";
+        string gameState = "start";
+
+        int leftClick, rightClick, upClick = 0;
+        bool leftDown, rightDown, upDown;
+
+        SolidBrush greenBrush = new SolidBrush(Color.Lime);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+
+        System.Windows.Media.MediaPlayer hit1 = new System.Windows.Media.MediaPlayer();
+        System.Windows.Media.MediaPlayer hit2 = new System.Windows.Media.MediaPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            player[0] = new Rectangle(205, 505, 40, 40);
+            player[1] = new Rectangle(155, 555, 40, 40);
+            player[2] = new Rectangle(205, 555, 40, 40);
+            player[3] = new Rectangle(255, 555, 40, 40);
+
+            for (int i = 0; i < 11; i++)
+            {
+                for (int ii = 0; ii < 9; ii++)
+                {
+                    squares[numHold] = new Rectangle(ii * 50 + 5, i * 50 + 5, 40, 40);
+                    numHold++;
+                }
+            }
+
+            hit1.Open(new Uri(Application.StartupPath + "/Resources/softHit.wav"));
+            hit2.Open(new Uri(Application.StartupPath + "/Resources/hardHit.wav"));
+        }
+
+        public void GameStart()
+        {
+            player[0] = new Rectangle(205, 505, 40, 40);
+            player[1] = new Rectangle(155, 555, 40, 40);
+            player[2] = new Rectangle(205, 555, 40, 40);
+            player[3] = new Rectangle(255, 555, 40, 40);
+
+            gameState = "running";
+            leftClick = 0;
+            rightClick = 0;
+            upClick = 0;
+            obstacleCount = 0;
+            shots.Clear();
+            playerScore = 0;
+            for(int i = 0; i < squaresActive.Count(); i++)
+            {
+                squaresActive[i] = 0;
+            }
+            gameEngine.Enabled = true;
+
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftDown = true;
+                    leftClick++;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    rightClick++;
+                    break;
+                case Keys.Up:
+                    upDown = true;
+                    upClick++;
+                    break;
+                case Keys.Space:
+                    if (gameState == "start" || gameState == "end")
+                    {
+                        gameState = "running";
+                        GameStart();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "start" || gameState == "end")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+                case Keys.D:
+                    if(gameState == "start" || gameState == "end")
+                    {
+                        switch (difficulty)
+                        {
+                            case "MEDIUM":
+                                difficulty = "HARD";
+                                break;
+                            case "HARD":
+                                difficulty = "EASY";
+                                break;
+                            default:
+                                difficulty = "MEDIUM";
+                                break;
+                        }
+                    }
+                    Refresh();
+                    break;
+            }
+        }
+
+        private void gameEngine_Tick(object sender, EventArgs e)
+        {
+            //establish speeds
+            switch (difficulty)
+            {
+                case "MEDIUM":
+                    obstacleSpeed = 30;
+                    break;
+                case "HARD":
+                    obstacleSpeed = 18;
+                    break;
+                default:
+                    obstacleSpeed = 42;
+                    break;
+            }
+
+            //move player
+            for (int i = 0; i < player.Length; i++)
+            {
+                if (leftDown == true && player[3].X > 55 && leftClick == 1)
+                {
+                    int x = player[i].X - playerSpeed;
+                    player[i] = new Rectangle(x, player[i].Y, 40, 40);
+                }
+
+                if (rightDown == true && player[3].X < this.Width - player[i].Width - 105 && rightClick == 1)
+                {
+                    int x = player[i].X + playerSpeed;
+                    player[i] = new Rectangle(x, player[i].Y, 40, 40);
+                }
+            }
+
+            //player shoots new square
+            if (upDown == true && upClick == 1)
+            {
+                shots.Add(new Rectangle(player[0].X, player[0].Y - 50, 40, 40));
+            }
+
+            //make buttons click not hold
+            leftClick = 0;
+            rightClick = 0;
+            upClick = 0;
+
+            //obstacles
+            obstacleCount++;
+
+            if (obstacleCount == obstacleSpeed)
+            {
+                //make obstacles move
+                for (int i = 98; i > 8; i--)
+                {
+                    squaresActive[i] = squaresActive[i - 9];
+                }
+
+                //make obstacles appear
+                for (int i = 0; i < 9; i++)
+                {
+                    numHold = randGen.Next(1, 21);
+                    if (numHold < 12)
+                    {
+                        squaresActive[i] = 1;
+                    }
+                    else
+                    {
+                        squaresActive[i] = 0;
+                    }
+                }
+
+
+                obstacleCount = 0;
+            }
+
+            //shot stops if necessary
+            for (int i = 0; i < squares.Length; i++)
+            {
+                for (int ii = 0; ii < shots.Count; ii++)
+                {
+                    if (shots[ii].X == squares[i].X && shots[ii].Y == squares[i].Y + 50 && squaresActive[i] == 1)
+                    {
+                        squaresActive[i + 9] = 1;
+                        shots.Remove(shots[ii]);
+                    }
+                    else if (shots[ii].Y == 5 && shots[ii].X == squares[i].X)
+                    {
+                        squaresActive[i] = 1;
+                        shots.Remove(shots[ii]);
+                    }
+                }
+            }
+
+            //shot moves
+            if (shots.Count > 0)
+            {
+                for (int i = 0; i < shots.Count; i++)
+                {
+                    int y = shots[i].Y - shotSpeed;
+                    shots[i] = new Rectangle(shots[i].X, y, 40, 40);
+                }
+
+            }
+
+            
+
+            //check to see if player has cleared row
+            for (int i = 0; i < 11; i++)
+            {
+                numHold = 0;
+                for(int ii = 0; ii < 9; ii++)
+                {
+                    if(squaresActive[i * 9 + ii] == 1)
+                    {
+                        numHold++;
+                    }
+                }
+                //clear row
+                if(numHold == 9)
+                {
+                    for(int ii = 0; ii < 9; ii++)
+                    {
+                        squaresActive[i * 9 + ii] = 0;
+                        
+                        for(int iii = 0; iii < 10 - i; iii++)
+                        {
+                            squaresActive[i * 9 + ii + iii * 9] = squaresActive[i * 9 + ii + iii * 9 + 9];
+                        }
+                        
+                    }
+                    playerScore = playerScore + 100;
+                    var pointSound = new System.Windows.Media.MediaPlayer();
+                    pointSound.Open(new Uri(Application.StartupPath + "/Resources/Point Get.wav"));
+                    pointSound.Play();
+                }
+
+            }
+
+            //check to see if player has died
+            for(int i = 0; i < 9; i++)
+            {
+                if (squaresActive[98 - i] == 1)
+                {
+                    Refresh();
+                    hit1.Stop();
+                    hit1.Play();
+                    Thread.Sleep(1500);
+                    greenBrush = new SolidBrush(Color.Red);
+                    scoreLabel.ForeColor = Color.Red;
+                    hit2.Stop();
+                    hit2.Play();
+                    Refresh();
+                    Thread.Sleep(1500);
+                    greenBrush = new SolidBrush(Color.Red);
+                    scoreLabel.ForeColor = Color.Lime;
+                    gameState = "end";
+                    gameEngine.Enabled = false;
+                }
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            switch (gameState)
+            {
+                case "start":
+                    //write titles
+                    titleLabel.Text = "SKYFALL64";
+                    subtitleLabel.Text = "Press space to play,\nd to change difficulty,\nor escape to exit.";
+                    difficultyLabel.Text = $"Difficulty: {difficulty}";
+                    scoreLabel.Text = "";
+                    break;
+                case "running":
+                    ////test draw squares
+                    //for(int i = 0; i < 99; i++)
+                    //{
+                    //    e.Graphics.FillRectangle(greenBrush, squares[i]);
+                    //}
+
+                    //draw squares
+                    for (int i = 0; i < 99; i++)
+                    {
+                        if (squaresActive[i] == 1)
+                        {
+                            e.Graphics.FillRectangle(greenBrush, squares[i]);
+                        }
+                        else
+                        {
+                            e.Graphics.FillRectangle(blackBrush, squares[i]);
+                        }
+                    }
+
+                    //draw player
+                    for (int i = 0; i < player.Length; i++)
+                    {
+                        e.Graphics.FillRectangle(greenBrush, player[i]);
+                    }
+                    
+                    //draw shots
+                    for(int i = 0; i < shots.Count; i++)
+                    {
+                        e.Graphics.FillRectangle(greenBrush, shots[i]);
+                    }
+
+                    //draw cover
+                    e.Graphics.FillRectangle(blackBrush, coverBack);
+                    e.Graphics.FillRectangle(greenBrush, coverLine);
+
+                    //erase titles
+                    titleLabel.Text = "";
+                    subtitleLabel.Text = "";
+                    difficultyLabel.Text = "";
+                    scoreLabel.Text = $"Score:\n{playerScore}";
+
+                    break;
+                case "end":
+                    //write titles
+                    titleLabel.Text = $"SCORE: {playerScore}";
+                    subtitleLabel.Text = "Press space to play,\nd to change difficulty,\nor escape to exit.";
+                    difficultyLabel.Text = $"Difficulty: {difficulty}";
+                    scoreLabel.Text = "";
+                    break;
+            }
+        }
+    }
+}
